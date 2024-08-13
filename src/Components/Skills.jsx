ae2129e954817bb4031c66adf25bbf28770c4dfa
Rohit Mali <rohitsmali9421@gmail.com>
2024-08-13 22:06:46 +0530

    .

---
+++ src/Components/Skills.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { FaCode } from "react-icons/fa";
+function Skills() {
+  return (
+    <div className='w-full bg-blue-50'>
+      <h1 className=' text-center font-bold text-3xl font-Kanit'> Skills < FaCode className='text-violet-500 inline text-5xl my-4' /></h1>
+      <div className='grid grid-cols-1 md:grid-cols-2'>
+        <img className='w-full bg-black justify-self-center' src="https://img.freepik.com/premium-vector/illustration-programmer-computer-vector-technology-concept-web-digital-design-programming_1013341-205260.jpg" alt="" />
+        <div className='mx-6 md:mx-12 p-4 md:p-6'>
+            <div>
+                <h1 className='font-Kanit text-orange-400 py-3 text-xl'>React</h1>
+                <div className='w-full bg-gray-300 h-2'>
+                    <div className='bg-violet-500 w-[90%] h-full'>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <h1 className='font-Kanit text-orange-400 py-3 text-xl'>React</h1>
+                <div className='w-full bg-gray-300 h-2'>
+                    <div className='bg-violet-500 w-[90%] h-full'>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <h1 className='font-Kanit text-orange-400 py-3 text-xl'>React</h1>
+                <div className='w-full bg-gray-300 h-2'>
+                    <div className='bg-violet-500 w-[90%] h-full'>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <h1 className='font-Kanit text-orange-400 py-3 text-xl'>React</h1>
+                <div className='w-full bg-gray-300 h-2'>
+                    <div className='bg-violet-500 w-[90%] h-full'>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <h1 className='font-Kanit text-orange-400 py-3 text-xl'>React</h1>
+                <div className='w-full bg-gray-300 h-2'>
+                    <div className='bg-violet-500 w-[90%] h-full'>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Skills
