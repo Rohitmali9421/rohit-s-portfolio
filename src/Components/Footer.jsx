3a7dd1fae2f29d2c0fbb79ff7cc2a2556919fce3
Rohit Mali <rohitsmali9421@gmail.com>
2024-08-14 22:49:10 +0530

    .

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='w-full bg-[#0b0d14] p-3 border-t border-gray-600'>
+        <h1 className='text-center text-white text-xl font-Kanit'>Made With ❤️ By Rohit</h1>
+    </div>
+  )
+}
+
+export default Footer
