7212d009d15fd7ee8b9daf8071bfda9a2c9b597f
Rohit Mali <rohitsmali9421@gmail.com>
2024-08-14 22:49:26 +0530

    .

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,51 +1,55 @@
 import React from 'react'
 import { FaCode } from "react-icons/fa";
 function Skills() {
-  return (
-    <div className='w-full bg-blue-50'>
-      <h1 className=' text-center font-bold text-3xl font-Kanit'> Skills < FaCode className='text-violet-500 inline text-5xl my-4' /></h1>
-      <div className='grid grid-cols-1 md:grid-cols-2'>
-        <img className='w-full bg-black justify-self-center' src="https://img.freepik.com/premium-vector/illustration-programmer-computer-vector-technology-concept-web-digital-design-programming_1013341-205260.jpg" alt="" />
-        <div className='mx-6 md:mx-12 p-4 md:p-6'>
-            <div>
-                <h1 className='font-Kanit text-orange-400 py-3 text-xl'>React</h1>
-                <div className='w-full bg-gray-300 h-2'>
-                    <div className='bg-violet-500 w-[90%] h-full'>
+    return (
+        <div className='w-full bg-blue-50'>
+            <h1 className=' text-center font-bold text-3xl font-Kanit'> Skills < FaCode className='text-violet-500 inline text-5xl my-4' /></h1>
+            <div className='grid grid-cols-1 md:grid-cols-2'>
+                <img className=' m-4 md:m-6 bg-black justify-self-center' src="https://www.shutterstock.com/image-vector/engineer-programmer-working-desk-programming-600nw-1234857673.jpg" alt="" />
+                <div className='mx-6 md:mx-12 p-4 md:p-6'>
+                    <div>
+                        <h1 className='font-Kanit text-orange-400 py-3 text-xl'>HTML</h1>
+                        <div className='w-full bg-gray-300 h-2'>
+                            <div className='bg-violet-500 w-[90%] h-full'>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div>
-                <h1 className='font-Kanit text-orange-400 py-3 text-xl'>React</h1>
-                <div className='w-full bg-gray-300 h-2'>
-                    <div className='bg-violet-500 w-[90%] h-full'>
+                    <div>
+                        <h1 className='font-Kanit text-orange-400 py-3 text-xl'>CSS</h1>
+                        <div className='w-full bg-gray-300 h-2'>
+                            <div className='bg-violet-500 w-[90%] h-full'>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div>
-                <h1 className='font-Kanit text-orange-400 py-3 text-xl'>React</h1>
-                <div className='w-full bg-gray-300 h-2'>
-                    <div className='bg-violet-500 w-[90%] h-full'>
+                    <div>
+                        <h1 className='font-Kanit text-orange-400 py-3 text-xl'>JavaScript</h1>
+                        <div className='w-full bg-gray-300 h-2'>
+                            <div className='bg-violet-500 w-[90%] h-full'>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div>
-                <h1 className='font-Kanit text-orange-400 py-3 text-xl'>React</h1>
-                <div className='w-full bg-gray-300 h-2'>
-                    <div className='bg-violet-500 w-[90%] h-full'>
+                    <div>
+                        <h1 className='font-Kanit text-orange-400 py-3 text-xl'>Java</h1>
+                        <div className='w-full bg-gray-300 h-2'>
+                            <div className='bg-violet-500 w-[90%] h-full'>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div>
-                <h1 className='font-Kanit text-orange-400 py-3 text-xl'>React</h1>
-                <div className='w-full bg-gray-300 h-2'>
-                    <div className='bg-violet-500 w-[90%] h-full'>
+                    <div className='grid justify-end grid-cols-4 gap-3 py-4 '>
+                        <img className='w-20' src="https://cdn-icons-png.freepik.com/256/17003/17003397.png?ga=GA1.1.1574181676.1712732853&semt=ais_hybrid" alt="" />
+                        <img className='w-20' src="https://t3.ftcdn.net/jpg/03/52/67/82/240_F_352678266_NFcwIwhhY76mkQItT4lCxyxcCTP3LgvY.jpg" alt="" />
+                        <img className='w-20' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuMRzP2i8h_xIFrtbyJvXy29eTCtyUpwIrzg&s" alt="" />
+                        <img className='w-20' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXpCVrym3sQKRS3_hAnLQIocLuMJorvv0Jiw&s" alt="" />
+                        <img className='w-20' src="https://bourhaouta.gallerycdn.vsassets.io/extensions/bourhaouta/tailwindshades/0.0.5/1592520164095/Microsoft.VisualStudio.Services.Icons.Default" alt="" />
+                        <img className='w-20' src="https://w7.pngwing.com/pngs/359/1024/png-transparent-firebase-cloud-messaging-computer-icons-google-cloud-messaging-android-angle-triangle-computer-programming-thumbnail.png" alt="" />
+                        <img className='w-20' src="https://cdn.iconscout.com/icon/free/png-256/free-postman-logo-icon-download-in-svg-png-gif-file-formats--technology-social-media-company-brand-vol-5-pack-logos-icons-2945092.png?f=webp&w=256" alt="" />
+                        <img className='w-20' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGRVQj408at1OZZOGxBEJYjs6g-Jih2HUGBQ&s" alt="" />
+                        
                     </div>
                 </div>
             </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default Skills
