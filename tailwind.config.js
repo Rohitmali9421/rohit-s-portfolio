2a8180476573f650498a43be02949ab4bee2ebc1
Rohit Mali <rohitsmali9421@gmail.com>
2024-08-17 19:38:39 +0530

    image fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,25 @@ export default {
       },
       boxShadow: {
         '1xl': '0px 0px 30px -15px rgba(0, 0, 0, 0.3)',
-      }
-    },
+      },
+      animation: {
+        'bounce-slow': 'bounceslow 3s linear infinite',
+      },
+      keyframes: {
+        bounceslow: {
+          '0%': {
+            transform: 'translateY(-5%)',
+          },
+          '50%': {
+            transform: 'translateY(0)',
+          },
+          '100%': {
+            transform: 'translateY(-5%)', 
+          },
+        },
+      },
+      
+    }
   },
   plugins: [],
 };
